c597f7488bf4c861e5fd6d1dc5e0508cf52800c1
a-lawn-flamingo <ywan1115@ucsc.edu>
2023-02-08 20:32:44 -0800

    add: double side texture

--- src/script.js
+++ src/script.js
@@ -5,30 +5,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
  * Textures
  */
 
-/* Using native Javascript */
-// const image = new Image()
-// const texture = new THREE.Texture(image)
-
-// image.onload = () =>
-// {
-//     texture.needsUpdate = true
-// }
-// image.src = '/textures/door/color.jpg'
-
-/* Using TextureLoader */
-// const textureLoader = new THREE.TextureLoader()
-// const texture = textureLoader.load(
-//     '/textures/door/color.jpg',
-//     // ()=>{
-//     //     console.log('loaded successfully')
-//     // },
-//     // ()=>{
-//     //     console.log('loading...') //3 functions after the path
-//     // },
-//     // ()=>{
-//     //     console.log('error')
-//     // },
-//     )
 
 /**
  * Base
@@ -44,6 +20,7 @@ const scene = new THREE.Scene()
  */
 const geometry = new THREE.TorusKnotGeometry( 10, 3, 100, 16 );
 const material = new THREE.MeshNormalMaterial()
+material.side = THREE.DoubleSide
 const mesh = new THREE.Mesh(geometry, material)
 scene.add(mesh)
 
